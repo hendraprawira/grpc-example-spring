0700bfaf5ea81a28389420ee7fca01c2bce23a58
hendra.prawira <hendra.ganda.k@len.co.id>
2024-04-30 10:47:15 +0700

    first commit

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@GrpcService
+public class DemoController extends HelloServiceGrpc.HelloServiceImplBase{
+
+
+    @Override
+    public void hello(HelloRequest request, StreamObserver<HelloResponse> responseObserver) {
+        HelloResponse reply = HelloResponse.newBuilder()
+                .setGreeting("Hello ==> " + request.getName())
+                .build();
+        responseObserver.onNext(reply);
+        responseObserver.onCompleted();
+    }
+}
